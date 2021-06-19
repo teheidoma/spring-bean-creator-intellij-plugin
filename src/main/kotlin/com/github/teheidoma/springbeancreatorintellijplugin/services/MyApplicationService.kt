bd2a4411bc3f31183dba770fcf3856054eb1aad3
GitHub Action <action@github.com>
2021-06-19 12:15:36 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/teheidoma/springbeancreatorintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.teheidoma.springbeancreatorintellijplugin.services
+
+import com.github.teheidoma.springbeancreatorintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/teheidoma/springbeancreatorintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/teheidoma/springbeancreatorintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.teheidoma.springbeancreatorintellijplugin.services
 
+import com.github.teheidoma.springbeancreatorintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
